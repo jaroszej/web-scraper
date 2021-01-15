a85ce68284be33c6b2e946279ea224ab010b5995
Jason Jaroszewicz <58184997+jaroszej@users.noreply.github.com>
2021-01-14 20:54:15 -0500

    Tweepy API setup
    Bot uses Tweepy to send wikipedia article tidbits companied with a link

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # web scraper
- 
-# TODO
-twitter API
 
-==> tweet main page article
+ https://twitter.com/RandomWikiBot2
+
+# README
+- see auth.txt for authorization setup
+
+# TODO
+twitter API - Tweepy
 
 ==> respond to replies
 
 - find "subject"
   
 - replies with link to "subject" and first 50 words...
+
+Automation
+
+==> send tweets periodically
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-# selenium web scraper app
+# python imports 
+import time
 
+# selenium imports
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-import time
+
+# tweepy imports
+import tweepy
+from auth import Authorization
+
+authKeys = Authorization()
+
+auth = tweepy.OAuthHandler(authKeys.consumer_token, authKeys.consumer_secret)
+auth.set_access_token(authKeys.access_token, authKeys.access_token_secret)
+
+api = tweepy.API(auth)
 
 TWITTER_CHARS = 280
 
@@ -17,7 +29,7 @@ driver = webdriver.Chrome(PATH)
 
 # wikipedia main page
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
-print(driver.title)
+wikiTitle = str(driver.title)
 
 # find Random article list item
 # return article title
@@ -25,8 +37,9 @@ randomArticle = driver.find_element_by_link_text("Random article")
 randomArticle.send_keys(Keys.RETURN)
 firstHeading = driver.find_element_by_id("firstHeading")
 # deduct # of chars from wiki title from # of available chars to tweet
-TWITTER_CHARS = TWITTER_CHARS - len(firstHeading.text)
-print(firstHeading.text)
+wikiHeading = str(firstHeading.text)
+TWITTER_CHARS = TWITTER_CHARS - len(wikiHeading)
+print(wikiHeading + "\n")
 
 # find first paragraph
 mwParserOutput = driver.find_element_by_class_name("mw-parser-output")
@@ -37,13 +50,23 @@ articleList = splitChars(articleContent)
 # wiki link & deduct length of link from # of available chars to tweet
 permLink = str(driver.current_url)
 TWITTER_CHARS = TWITTER_CHARS - len(permLink)
+print(str(TWITTER_CHARS) + " available characters to tweet with\n")
 
-# print wiki article with space left for tweet
-articleListSmaller = articleList[:(TWITTER_CHARS - 15)]
+# wiki article with space left for tweet
+articleListSmaller = articleList[:(TWITTER_CHARS)]
 articleStr = ''.join(articleList)
-print(articleStr, end ="")
-print("...")
+elipse = "..."
+wikiArticle = articleStr + elipse
 readMore = "Read more: "
-print(readMore + permLink)
 
 driver.quit()
+
+# formats tweet
+with open('temptweet.txt', 'w', encoding="utf-8") as f:
+    f.write(wikiHeading + '\n\nFrom Wikipedia . . .\n\n' + wikiArticle + '\n' + readMore + '\n' + permLink)
+
+# sends tweet
+with open('temptweet.txt', 'r') as f:
+    api.update_status(f.read())
+
+print("Sent " + wikiHeading + " Wikipedia article as a tweet. . .")
